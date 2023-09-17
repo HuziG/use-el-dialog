432d4807e38345f2318859c7609d007ac2c95563
xiaoxuanzhang <1067408814@qq.com>
2023-09-17 20:49:18 +0800

    示例

--- example/App.vue
+++ example/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
-    hello use dialog
+    <el-button @click="handleOpenDialog">Open</el-button>
+
+    <basic-modal @register="useRegister"></basic-modal>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useElDialog } from '../package/src/hooks/useElDialog'
+
+const [useRegister, useMethods] = useElDialog({
+  title: 'new Use El Dialog',
+})
+
+const handleOpenDialog = () => {
+  useMethods.openModal()
+}
+</script>
--- example/main.ts
+++ example/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import UseElDialog from '../package/index';
+import 'element-plus/dist/index.css';
+import { ElDialog, ElButton } from 'element-plus';
+
 const app = createApp(App);
+
+app.use(UseElDialog);
+app.use(ElDialog);
+app.use(ElButton);
 
 app.mount('#app');
