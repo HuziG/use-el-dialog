7bfa5d273369c34c6b85bd0cb7c547b49a2ee64a
xiaoxuanzhang <1067408814@qq.com>
2023-10-15 09:30:49 +0800

    Update global-shim.d.ts

--- package/global-shim.d.ts
+++ package/global-shim.d.ts
@@ -1,3 +1,13 @@
+// env.d.ts
+import BasicElDialog from './package/src/dialog.vue';
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    BasicElDialog: typeof BasicElDialog;
+  }
+}
+
+// 2. Declare a module use-el-dialog
 declare module 'use-el-dialog' {
   export function useElDialog(): any;
 }
