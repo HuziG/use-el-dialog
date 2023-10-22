e393f403d668eb1c9adcd33adca8d2da00b5da6c
xiaoxuanzhang <1067408814@qq.com>
2023-10-22 16:00:29 +0800

    type

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue', 'element-plus'],
       output: {
+        name: 'use-el-dialog',
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
