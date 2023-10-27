7ee05332a399aed4d317809f54cf343e6651310c
HuziG <1067408814@qq.com>
2023-10-27 14:34:35 +0800

    global options

--- package/index.ts
+++ package/index.ts
@@ -8,8 +8,8 @@ const config = {}
 
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 export function install(app: any, options?: Partial<NewDialogProps>) {
-  if (app.$_vUseElDialogInstalled) return
-  app.$_vUseElDialogInstalled = true
+  // if (app.$_vUseElDialogInstalled) return
+  // app.$_vUseElDialogInstalled = true
 
   assign(config, options || {})
 
